9ef695c0ec677b6dde2bd985c200d14dd3ab1dab
Maria <msawaby@gmail.com>
2017-04-15 14:16:20 -0700

    final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
 
-var selectedPlayerID;
-var selectedEnemyID;
-var counterAttackPower = 0;
 
 
+//creating and initializing object characters
 var hansoloChar = {
 	imgSrc : "assets/images/hansolo.png",
 	healthPoints: 130,
@@ -28,44 +26,45 @@ var legoChar = {
 	name: "Lego Char",
 	attackPower: 10
 };
-//var attackPower;
-var playerHp;
+
+//array which contains every object
 var charArray = [hansoloChar, mugChar, chewChar, legoChar];
+//variables
+var selectedPlayerID;
+var selectedEnemyID;
+var counterAttackPower = 0;
+var playerHp;
 var container;
-var enimyCounterHp =0;
 var healthP;
 var isEnemySelected = false;
 
+//this function creates and sets all initial values to the DOM
 function charCreation(id, charArray){
 	for(var i = 0; i < charArray.length; i++) {
-		//TODO handle p tag, and append everything to the contatiner and then the dom
 		container = $("<div>");
 		container.addClass("thecont");
 		var img = $("<img>");
 		img.addClass("boxImage");
-		console.log("attack power of first"+charArray[i].attackPower);
-		//container.attr("data-attackPower", charArray[i].attackPower);
-		//container.attr("data-hpValue",  charArray[i].healthPoints);
+		//setting all 3 attributes to the container
 		container.attr({
 			"data-attackPower": charArray[i].attackPower,
 			"data-hpValue":  charArray[i].healthPoints,
 			"data-name": charArray[i].name
 		});
-		container.attr("id", i);
-		console.log(container.attr("data-attackPower"));
-		//console.log(charArray[i].attackPower)
+		//container.attr("id", i);
 		img.attr("src", charArray[i].imgSrc);
-		// p tag
+		// p tag for name
 		var nameP = $("<p>")
-
 		nameP.addClass("paragraph");
 		nameP.text(charArray[i].name);
+		// setting health point attribute for each charachter
 		healthP = $("<p>");
 		healthP.addClass("paragraph health-p");
 		healthP.text(charArray[i].healthPoints);
 		container.append(nameP);
 		container.append(img);
 		container.append(healthP);
+		// appending everything in the DOM
 		$(id+(i+1)).append(container);
 	}
 }
@@ -77,95 +76,83 @@ var youName;
 var enimyName;
 var enimyHp ;
 
-// when character is clicked add it to a variable called selectedChar
-// then add the other characters to a array called otherChars
-// then append them to the neccessary containers
+// selecting character
 $(".thecont").parent().on("click", function(){
-	//console.log("the object"+ $(this).children());
+	
     var id = $(this).attr("id");
     selectedPlayerID = id;
-    //console.log("idobject"+id);
+
     var num = id.split("-")[1]-1;
-   // console.log(num);
     var selectedChar = charArray[num];
     attackPower = parseInt($(this).children().attr("data-attackPower"));
     playerHp = parseInt($(this).children().attr("data-hpValue"));
-    console.log("playerHp:" + playerHp);
-   // youName = $(this).children().attr("data-name");
-   // console.log("attackPower"+attackPower);
+   	// finding which element is selected, then put other unselected into a new array called enemies
     index = charArray.indexOf(selectedChar);
-    //console.log("selectedChar"+selectedChar);
-    
     charArray.splice(index, 1);
-   // console.log("charArray"+charArray);
+   // assigning enemies into new array "enimies"
     for (var i = 0; i < charArray.length; i++) {
     	if(charArray[i] != selectedChar) {
     		enimies.push(charArray[i]);
     	}
     }
-    console.log(enimies);
+   	// replace all characters with selected character
     $('#map').replaceWith($(this));
     $(".invisibleBox").show();
     $(this).off("click");
     charCreation("#notselectedchar-", enimies);
-    //console.log($(this));
-	
-	//healthPoints = parseInt($(this).children().attr("data-hpValue"));
-	
+    
 });	
 
 
 var counterEnimy=0;
-// invisible click enimies
-$(".invisibleBox").on("click", function(){
+// click enimies, this function is to set enemy attributes and define defender
+function enemySelection(){
+//$(".invisibleBox").on("click", function(){
 	$("#attackCounter").html('');
-	console.log("the object enimy"+ $(this).children());
+	
 	counterEnimy++;
     var id = $(this).attr("id");
     selectedEnemyID = id;
-    //console.log("idobject"+id);
+    
     var num = id.split("-")[1]-1;
-    //console.log(num);
+ 
     var oponent = enimies[num];
     enimyAttackPower = parseInt($(this).children().attr("data-attackPower"));
     enimyName = $(this).children().attr("data-name");
     enimyHp = parseInt($(this).children().attr("data-hpValue"));
-    // console.log("attackPower enimy"+enimyAttackPower);
-    console.log("hp enimy type"+typeof(enimyHp));
+    
+   
     index = enimies.indexOf(oponent);
-    //console.log("selectedChar"+selectedChar);
+   
     var enimy = [];
     enimies.splice(index, 1);
-    //console.log("charArray"+enimies);
+
     for (var i = 0; i < enimies.length; i++) {
     	if(enimies[i] != oponent) {
     		enimy.push(enimies[i]);
     	}
     }
-    console.log("enimies"+enimy);
-    // change html with replace or append if necessary
+    
     $('#defender').html($(this));
-    //$(".defenderBox").show();
+   // changing style of defender
     $(this).css({
       "background-color": "black",
       "border": "1.5px solid red",
       "color": "#fff"
     });
-   // $(this).off("click");
 	
 	isEnemySelected = true;
+	$(".invisibleBox").off("click");
+}	
 
-});	
-
-// TODO: to count counterAttackPower and calculate healthpoint and change the property of object
 var parAttack = $("<p>");
 parAttack.attr("id", "parAttackStyle");
 var newHP =0;
 //this paragraph is created to show the defeats, attacks lose and wins
 var defeat = $("<p>");
 defeat.attr("id", "parAttackStyle");
-//parAttack.addClass("paragraph");
 //Attack Button
+
 $("#attackButton").on("click", function(){
 	
 	if (isEnemySelected) {
@@ -176,27 +163,14 @@ $("#attackButton").on("click", function(){
 		counterAttackPower = counterAttackPower + attackPower;
 		// calculates health points of enimy
 		enimyHp = enimyHp-counterAttackPower;
-		console.log("enimy health point"+enimyHp);
 		playerHp = playerHp-enimyAttackPower;
 		parAttack.text("You attacked "+enimyName+" By "+counterAttackPower+" damage! \n ");
 		$("#attackCounter").html('');
 		$("#attackCounter").append(parAttack);
-		//<br>"+enimyName+"\t Attacked you By "+enimyAttackPower+" damage!");
 		parAttack.append(enimyName+" Attacked you By "+enimyAttackPower+" damage!");
-		// set new attr bor player health points
-		//var newhpofplayer = $("div#map").children(".boxImage").attr("data-hpValue", playerHp);
-		//newhpofplayer.text(playerHp);
-		//$("#"+).find(".health-p").text()
+		
 		$("#"+selectedPlayerID).find(".health-p").text(playerHp);
 		$("#"+selectedEnemyID).find(".health-p").text(enimyHp);
-		// $("div#map").children().text(healthP.text(playerHp));
-		//console.log("data-hpValue of new player "+newhpofplayer);
-		//$("#map").prop(healthPoints, playerHp);
-		//console.log("health point attack player"+playerHp);
-
-		//parAttack.text("health point of ob is"+healthPoints);
-
-
 
 		// here should check how much the player and enimy's health point are
 		if(playerHp <= 0){
@@ -208,9 +182,11 @@ $("#attackButton").on("click", function(){
 			$(".invisibleBox").off("click");
 			$(this).off("click");
 		} 
-
+		// if enemy health point is less than zero then the player defeated the 
+		// enemy and should be able to choose a new defender
 		if(enimyHp <=0){
 			$("#defender").html('');
+			$(".invisibleBox").on("click", enemySelection);
 			isEnemySelected = false;
 			if(enimyHp <=0 && playerHp >0){
 				defeat.text("You have defeated "+enimyName+", you can choose to fight another enemy.");
@@ -218,20 +194,19 @@ $("#attackButton").on("click", function(){
 			}else{
 				defeat.text("You have defeated "+enimyName+", You have also been defeated, Play Again.");
 				$("#attackCounter").html(defeat);
+				$(".invisibleBox").off("click");
 			}
 		}
 	} else {
 		defeat.text("Please select an enemy before attacking!");
 		$("#attackCounter").html(defeat);
-		//alert('Please select an enemy before attacking!');
 	}
 	if(counterEnimy ==2 && enimyHp <= 0){
 		var audio = new Audio('./assets/policepass.wav');
 		audio.play();
 	}
-	//Game Over!
+	//Game Over in case of defeating all enemies!
 	if(counterEnimy == 3 && enimyHp <= 0){
-		console.log("game over "+ counterEnimy);
 		defeat.text("You Won.....Game Over!");
 		$("#attackCounter").html(defeat);
 		$("#resetButton").show();
@@ -239,6 +214,8 @@ $("#attackButton").on("click", function(){
 	}
 
 });//end of attack button on click
+//calling the enemySelection function to enable player to select an enemy
+$(".invisibleBox").on("click", enemySelection);
 
 // reset button function
 $("#resetButton").on("click", function(){
